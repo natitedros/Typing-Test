bef42ba778b174b6e5c02d6c7a34fc7a2d9036b2
Nati_Tedros <tedros.natnael@gmail.com>
2024-04-21 22:47:47 -0400

    added fetch api with unfixed bug

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -1,7 +1,13 @@
 import TestContent from "./testContent";
+import useFetch from "./useFetch";
 
 interface HomeProps {
   isLightThemed: boolean;
+}
+interface DefinitionModel {
+  definition: string;
+  word: string;
+  valid: boolean;
 }
 
 const Home: React.FunctionComponent<HomeProps> = (props) => {
@@ -19,6 +25,19 @@ const Home: React.FunctionComponent<HomeProps> = (props) => {
     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
     sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+  const words = [
+    "abate",
+    "bellicosity",
+    "confound",
+    "abstruse",
+    "bane",
+    "crave",
+  ];
+
+  const { data, isPending, error } = useFetch<DefinitionModel>(
+    "https://api.api-ninjas.com/v1/dictionary?word=" + words[0]
+  );
 
   return (
     <div>
---
+++ frontend/src/useFetch.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+const useFetch = <T>(url: string) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const apiKey = process.env.REACT_APP_API_NINJA_KEY;
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set("X-Api-Key", `${apiKey}`);
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data!");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted!");
+        } else {
+          setError(err.message);
+          setIsPending(false);
+        }
+      });
+    return () => abortCont.abort();
+  }, [url, apiKey]);
+  return [data, isPending, error];
+};
+export default useFetch;
