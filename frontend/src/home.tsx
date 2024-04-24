1f0054a55bdaa9ae0d1274bc802327c59cd3e446
Nati_Tedros <tedros.natnael@gmail.com>
2024-04-24 00:16:13 -0400

    added color update on entered letters

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -13,18 +13,7 @@ interface DefinitionModel {
 const Home: React.FunctionComponent<HomeProps> = (props) => {
   const isLightThemed = props.isLightThemed;
 
-  const sample = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-    sunt in culpa qui officia deserunt mollit anim id est laborum.
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-    sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+  const sample = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
 
   const words = [
     "abate",
--- frontend/src/testContent.tsx
+++ frontend/src/testContent.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface TestContentProps {
   paragraph: string;
   isLightThemed: boolean;
@@ -5,15 +7,46 @@ interface TestContentProps {
 
 const TestContent: React.FunctionComponent<TestContentProps> = (props) => {
   const paragraph = props.paragraph;
-  const isLight = props.isLightThemed;
-
-  let textColor = isLight ? "text-black" : "text-gray-400";
-  textColor += " transition-colors duration-500";
-
   const content = paragraph.slice(0, Math.min(paragraph.length, 1000));
+  const isLight = props.isLightThemed;
+  const [currIndex, setCurrIndex] = useState(0);
+  const [wordCount, setWordCount] = useState(0);
+  let textColor = isLight ? "text-gray-600" : "text-gray-400";
+  textColor += " transition-colors duration-500";
+  const trackTest = (char: string) => {
+    if (char === content[currIndex]) {
+      if (char === " ") {
+        setWordCount(wordCount + 1);
+      }
+      setCurrIndex(currIndex + 1);
+      console.log("yesss");
+    } else {
+      console.log("char: " + char);
+      console.log("current Letter: " + content[currIndex]);
+      console.log(currIndex);
+      console.log(content.slice(currIndex, currIndex + 10));
+      console.log("noooo");
+    }
+  };
+
+  const renderTextWithColoredLetter = () => {
+    return content.split("").map((letter, index) => {
+      return (
+        <span
+          key={index}
+          className={index === currIndex ? "text-yellow-500" : textColor}
+        >
+          {letter}
+        </span>
+      );
+    });
+  };
   return (
     <div className="mx-20 px-20 mt-20">
-      <article className={`${textColor} text-2xl`}>{content}</article>
+      <input value="" onChange={(e) => trackTest(e.target.value)} />
+      <article className={`${textColor} text-2xl`}>
+        {renderTextWithColoredLetter()}
+      </article>
     </div>
   );
 };
